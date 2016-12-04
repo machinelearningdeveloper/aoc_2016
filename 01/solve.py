d6acd4324f5fe5e57750a335b35cd42edd8544b5
Tom Lynch <tom@machinelearningdeveloper.com>
2016-12-04 14:11:57 -0600

    Solve the puzzle.

---
+++ 01/solve.py
@@ -0,0 +1,18 @@
+"""Report the manhattan distance between a starting point and an ending point,
+given a set of directions to follow to get move between the two points."""
+
+
+from distance import get_distance
+from directions import load_directions, follow_directions
+
+
+def main():
+    directions = load_directions('directions.txt')
+    starting_point = (0, 0)
+    starting_orientation = 'N'
+    ending_point, _ = follow_directions(starting_point, starting_orientation, *directions)
+    print(get_distance(starting_point, ending_point))
+
+
+if __name__ == '__main__':
+    main()
