a2f4b30cab3dafe119e42181772f4d77b575ec0e
Tom Lynch <tom@machinelearningdeveloper.com>
2016-12-17 21:24:37 -0600

    Add test for part 2 of day 5.

--- 05/test_find_password.py
+++ 05/test_find_password.py
@@ -6,3 +6,4 @@ from find_password import find_password
 class TestFindPassword(unittest.TestCase):
     def test_find_password(self):
         assert find_password('abc', length=8) == '18f47a30'
+        assert find_password('abc', length=8, complex=True) == '05ace8e3'
