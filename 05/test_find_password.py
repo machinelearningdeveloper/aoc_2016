f507f1e642db70641940da340c870cc51e7fd78f
Tom Lynch <tom@machinelearningdeveloper.com>
2016-12-17 21:12:52 -0600

    Add test for find_password.

---
+++ 05/test_find_password.py
@@ -0,0 +1,8 @@
+import unittest
+
+from find_password import find_password
+
+
+class TestFindPassword(unittest.TestCase):
+    def test_find_password(self):
+        assert find_password('abc', length=8) == '18f47a30'
